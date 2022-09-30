4c1b954639439573dee822dee09cc539eb13b61f
Lucas <lucas2carlos@gmail.com>
2022-09-30 16:00:23 -0400

    building Api

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log('testing JS')
+
+///////////////////////////// API ///////////////////////////
+
+const Api = (() => {
+    const baseURL = 'https://randomuser.me/api'
+
+    async function getUser() {
+        const res = await fetch(baseURL)
+        const data = await res.json()
+        console.log(data.results)
+        return await data.results
+    }
+
+    return {getUser}
+})()
+
+console.log(fetch(baseURL))
+console.log(Api.getUser())
