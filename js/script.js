3a402394e12243051b48112c5a49b9c5a58cdec7
Lucas <lucas2carlos@gmail.com>
2022-10-02 17:29:51 -0400

    project

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@
 
 ///////////////////////////////////////// PAGE LOADS AFTER A FEW SECONDS DELAY ////////////////////////////////////
 
+
+const generateRandomId = () => {
+    const reuse = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz"
+    const length = 12;
+    let id = ''
+    for (let i = 0; i < length; i++){
+        const index = Math.floor(Math.random() * reuse.length)
+        id +=reuse[index]
+    }
+    return id
+}
+
 ///////////////////////////// API ///////////////////////////
 
 const Api = (() => {
@@ -10,7 +22,6 @@ const Api = (() => {
     async function getUser() {
         const res = await fetch(baseURL)
         const data = await res.json()
-        // console.log(data.results)
         return await data.results
     }
 
@@ -23,19 +34,19 @@ const Api = (() => {
 
 const View =(() => {
     const domStr = {
-        usersContainer: "#usersContainer"
+        usersContainer: "#usersContainer",
+        dobDiv: '.dobDiv'
     }
     const render = (ele, tmplt) => {
         ele.innerHTML = tmplt
     }
 
     const createTmplt = (arr) => {
-        // console.log('create function')
-        // console.log(arr)
         let tmplt = ''
         arr.forEach((users) => {
             // console.log(arr)
             for(let ele of users){
+                ele.id = generateRandomId()
                 tmplt += `
                 <li>
                     <img src= " ${ele.picture.large}" alt="">
@@ -43,7 +54,10 @@ const View =(() => {
                         <span>name: ${ele.name.title}. ${ele.name.last}</span>
                         <span>email: ${ele.email}</span>
                         <span>phone: ${ele.cell}</span>
+                        <span id="${ele.id}" style="display: none">phone: ${ele.dob.date}</span>
+                        <div id="${ele.id}" class="dobDiv">
                         <button>Show Dob</button>
+                        </div>
                     </div>
                 </li>
                 `
@@ -87,23 +101,6 @@ const Model = ((api, view) => {
 
 const Controller = ((model, view) => {
     const state = new model.State()
-    // const init = () => {
-    //     console.log('init')
-    //     // let user = []
-    //     // for(let i = 0; i < 20; i++){
-    //         model.getUser().then((person) => {
-    //             console.log(person[0])
-    //             // user = [...user, person[0]]
-    //             state.userList = [...userList, person[0]]
-    //             // console.log(user)
-    //             // console.log(state.userList)
-    
-    //         })
-    //     // }
-    //     // const getUser = model.getUser()
-    //     // console.log('user array: ' + user.length)
-
-    // }
     let user = {}
     async function init() {
         let users = []
@@ -112,18 +109,33 @@ const Controller = ((model, view) => {
             users.push(user)
         }
         state.userList = users
-        // console.log(users)
+    }
+
+    const showFn = () => {
+        const dobDiv = document.querySelectorAll(view.domStr.dobDiv)
+        for(let div of dobDiv){
+            div.addEventListener("click", (evt) => {
+                let span = document.querySelector(`span#${div.id}`)
+                if(div.innerHTML !== span.innerHTML){
+                    div.innerHTML = span.innerHTML
+                } else{
+                    div.innerHTML = '<button>Show Dob</button>'
+                }
+            })
+        }
     }
 
 
-    const run = () => {
-        init()
+    async function run () {
+        await init()
+        await showFn()
     }
 
     return {run}
-})(Model)
+})(Model, View)
 
 Controller.run()
+
 
 
 
